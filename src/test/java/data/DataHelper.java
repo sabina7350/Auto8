ee5233f461420042ec691f75b47a2e1a5dc39cf2
sabibi_k <sabina7350@gmail.com>
2024-06-06 21:36:02 +0300

    InitialCommit

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,52 @@
+package data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    public static final Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfoWithTestData() {
+
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static String generateRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String generateRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode generateRandomVerificationCode() {
+        return new VerificationCode(faker.numerify("######"));
+
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String testId;
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+}
---
+++ src/test/java/test/BankLoginTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import data.DataHelper;
+import data.SQLHelper;
+import page.LoginPage;
+
+import static data.SQLHelper.cleanDatabase;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+
+    }
+
+    @Test
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisibility();
+    }
+
+    @Test
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisibility();
+
+    }
+}
